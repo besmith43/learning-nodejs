f0e5ecd6f00e10dee918a3773fb17cde8a50006d
Blake Smith <besmith43@gmail.com>
2021-07-09 11:16:15 -0500

    added css modification from js

--- blank-template/js/main.js
+++ blank-template/js/main.js
@@ -2,3 +2,6 @@ console.log("hello");
 function ButtonClicked() {
     alert('You clicked the button');
 }
+function ChangeHeaderColor() {
+    document.getElementById("hello").style.color = "blue";
+}
--- blank-template/ts/main.ts
+++ blank-template/ts/main.ts
@@ -3,3 +3,7 @@ console.log("hello");
 function ButtonClicked() {
     alert('You clicked the button');
 }
+
+function ChangeHeaderColor() {
+    document.getElementById("hello").style.color = "blue";
+}
